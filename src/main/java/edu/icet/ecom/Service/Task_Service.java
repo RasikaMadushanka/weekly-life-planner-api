7b684208ae264728113b00d42857436784b0be6e
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-03-15 19:30:35 +0530

    make task service

--- src/main/java/edu/icet/ecom/Service/Task_Service.java
+++ src/main/java/edu/icet/ecom/Service/Task_Service.java
@@ -1,4 +1,15 @@
 package edu.icet.ecom.Service;
 
+import edu.icet.ecom.Model.Dto.Task_dto;
+import edu.icet.ecom.Repository.Task_Repository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Task_Service {
+    @Autowired
+    private Task_Repository taskRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+    public Task_dto saveTask(Task_dto taskDto) {
+    }
 }
